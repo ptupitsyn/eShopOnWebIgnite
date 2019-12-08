41cc32382af13859cdffac8d0a9c991b5d88bd83
Pavel Tupitsyn <ptupitsyn@apache.org>
2019-12-08 23:29:16 +0300

    Fixing tests

--- tests/UnitTests/ApplicationCore/Specifications/CatalogFilterSpecificationFilter.cs
+++ tests/UnitTests/ApplicationCore/Specifications/CatalogFilterSpecificationFilter.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.eShopWeb.ApplicationCore.Specifications;
+﻿using System;
+using Microsoft.eShopWeb.ApplicationCore.Specifications;
 using Microsoft.eShopWeb.ApplicationCore.Entities;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,8 @@ namespace Microsoft.eShopWeb.UnitTests
 {
     public class CatalogFilterSpecificationFilter
     {
+        private static readonly Guid[] Ids = Enumerable.Range(1, 10).Select(_ => Guid.NewGuid()).ToArray();
+        
         [Theory]
         [InlineData(null, null, 5)]
         [InlineData(1, null, 3)]
@@ -18,7 +21,9 @@ namespace Microsoft.eShopWeb.UnitTests
         [InlineData(2, 3, 0)]
         public void MatchesExpectedNumberOfItems(int? brandId, int? typeId, int expectedCount)
         {
-            var spec = new CatalogFilterSpecification(brandId, typeId);
+            var spec = new CatalogFilterSpecification(
+                brandId == null ? (Guid?) null : Ids[brandId.Value],
+                typeId == null ? (Guid?) null : Ids[typeId.Value]);
 
             var result = GetTestItemCollection()
                 .AsQueryable()
@@ -31,11 +36,11 @@ namespace Microsoft.eShopWeb.UnitTests
         {
             return new List<CatalogItem>
             {
-                new CatalogItem { CatalogBrandId = 1, CatalogTypeId= 1 },
-                new CatalogItem { CatalogBrandId = 1, CatalogTypeId= 2 },
-                new CatalogItem { CatalogBrandId = 1, CatalogTypeId= 3 },
-                new CatalogItem { CatalogBrandId = 2, CatalogTypeId= 1 },
-                new CatalogItem { CatalogBrandId = 2, CatalogTypeId= 2 },
+                new CatalogItem {CatalogBrandId = Ids[1], CatalogTypeId = Ids[1]},
+                new CatalogItem {CatalogBrandId = Ids[1], CatalogTypeId = Ids[2]},
+                new CatalogItem {CatalogBrandId = Ids[1], CatalogTypeId = Ids[3]},
+                new CatalogItem {CatalogBrandId = Ids[2], CatalogTypeId = Ids[1]},
+                new CatalogItem {CatalogBrandId = Ids[2], CatalogTypeId = Ids[2]},
             };
         }
     }
